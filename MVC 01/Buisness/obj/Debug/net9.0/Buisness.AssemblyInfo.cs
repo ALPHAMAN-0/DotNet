342be5e964e70b5c6b3e48317a7be84c741c01e4
Siam <huffmancoding.acm@gmail.com>
2025-05-31 10:03:08 +0600

    no message

--- MVC 01/Buisness/obj/Debug/net9.0/Buisness.AssemblyInfo.cs
+++ MVC 01/Buisness/obj/Debug/net9.0/Buisness.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Buisness")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b9c3c9d9554184599360a6795aaead6f94ad2d22")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f0cc02ffe131ddadd522a00234d982a73ca06339")]
 [assembly: System.Reflection.AssemblyProductAttribute("Buisness")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Buisness")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
