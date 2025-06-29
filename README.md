f3cc9542ce6b9ab23f522fc74121f5c59f8b5a9a
Siam <huffmancoding.acm@gmail.com>
2025-06-29 11:46:51 +0600

    no message

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 # DotNet
 # .NET Framework
 
--- Web API/EcommerceWebAPI/Program.cs
+++ Web API/EcommerceWebAPI/Program.cs
@@ -1,10 +1,40 @@
 var builder = WebApplication.CreateBuilder(args);
 
-
-builder.Services.AddOpenApi();
+// Add services to the container
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+// Configure the HTTP request pipeline
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
 app.UseHttpsRedirection();
+app.UseAuthorization();
+
+// Map controllers
+app.MapControllers();
+
+// Minimal API endpoints
+app.MapGet("/siam", () =>
+{
+    return "Welcome to the Siam API!";
+});
+
+app.MapPost("/hello", () =>
+{
+    return "Hello from the Siam API!";
+});
+
+// Print out the available routes
+app.MapGet("/", () => 
+{
+    return "API is running. Available endpoints: /siam (GET), /hello (POST)";
+});
 
 app.Run();
