90b5480168575a03f1bd09cbce92b5a1a6094b0b
Siam <huffmancoding.acm@gmail.com>
2025-08-11 06:14:33 +0600

    no message

---
+++ C# /Test.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class InstantNoodles
+{
+    public static void Main(string[] args)
+    {
+       int number1 = int.Parse(Console.ReadLine());
+       int number2 = int.Parse(Console.ReadLine());
+       
+       int result = number1*number2;
+       Console.WriteLine(result);
+    }
+}
