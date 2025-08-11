b86c08d331892b17c4a3dd16d497ed8437f2fc37
Siam <huffmancoding.acm@gmail.com>
2025-08-11 08:18:44 +0600

    no message

--- C# /Test.cs
+++ C# /Test.cs
@@ -1,13 +1,120 @@
-using System;
 
-public class InstantNoodles
+ 
+
+class Device
+
 {
-    public static void Main(string[] args)
-    {
-       int number1 = int.Parse(Console.ReadLine());
-       int number2 = int.Parse(Console.ReadLine());
-       
-       int result = number1*number2;
-       Console.WriteLine(result);
-    }
+
+   public string Brand;
+
+   public Device(string brand)
+
+   {
+
+       Brand = brand;
+
+       Console.WriteLine($"Device {brand} created");
+
+   }
+
+   public float GetBasePrice()
+
+   {
+
+       return 100.0f;
+
+   }
+
+}
+
+ 
+
+class Phone : Device
+
+{
+
+   public int CameraCount;
+
+ 
+
+   public Phone(string brand) : base(brand)
+
+   {
+
+       CameraCount = 1;
+
+       Console.WriteLine($"Phone {brand} created");
+
+   }
+
+ 
+
+   public float GetPhonePrice()
+
+   {
+
+       return GetBasePrice() + CameraCount * 50;
+
+   }
+
+}
+
+ 
+
+ 
+
+ 
+
+class PriceChecker
+
+{
+
+   public static void ShowPrice(Device d)
+
+   {
+
+       Console.WriteLine($"Device Price: {d.GetBasePrice()}");
+
+ 
+
+ }
+
+ 
+
+   public static void ShowPrice(Phone p)
+
+   {
+
+       Console.WriteLine($"Phone Price:   {p.GetPhonePrice()}");
+
+ 
+
+   }
+
+}
+
+ 
+
+class Program
+
+{
+
+   static void Main()
+
+   {
+
+       Device d1 = new Device("GenericBrand");
+
+       Phone p1 = new Phone("Samsung");
+
+       //New Device(“Test”);
+
+ 
+
+       PriceChecker.ShowPrice(d1);
+
+       PriceChecker.ShowPrice(p1);
+
+   }
+
 }
